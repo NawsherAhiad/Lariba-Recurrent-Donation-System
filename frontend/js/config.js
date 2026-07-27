b8de82d5e603d336b3d7cc5806cc3357b336c58b
NawsherAhiad <ahiad.ewu@gmail.com>
2026-07-28 03:25:01 +0600

    Fix API_BASE_URL for production and break script cache

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,18 @@
+// const CONFIG = {
+//     API_BASE_URL: "http://127.0.0.1:8000/api/v1",
+
+//     ENDPOINTS: {
+//         DONATE: "/donations",
+//         QUERY_PAYMENT: "/bkash/query",
+//         SEARCH_TRANSACTION: "/bkash/search"
+//     }
+// };
+
+const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+
 const CONFIG = {
-    API_BASE_URL: "http://127.0.0.1:8000/api/v1",
+    // If local, use full localhost URL; otherwise use relative path ""
+    API_BASE_URL: isLocal ? "http://127.0.0.1:8000/api/v1" : "/api/v1",
 
     ENDPOINTS: {
         DONATE: "/donations",
